0a68d7d0c5cd36f60d56b2055b15b8387cac9943
audiesha brattin <a.brattin@gmail.com>
2021-04-25 20:16:56 -0500

    adding solutions again

--- src/main/java/com/codeup/adlister/dao/DaoFactory.java
+++ src/main/java/com/codeup/adlister/dao/DaoFactory.java
@@ -1,13 +1,13 @@
 package com.codeup.adlister.dao;
 
 public class DaoFactory {
-    private static Ads adsDao;
-    private static Config config = new Config();
+  private static Ads adsDao;
+  private static Config config = new Config();
 
-    public static Ads getAdsDao() {
-        if (adsDao == null) {
-            adsDao = new MySQLAdsDao(config);
-        }
-        return adsDao;
+  public static Ads getAdsDao() {
+    if (adsDao == null) {
+      adsDao = new MySQLAdsDao(config);
     }
+    return adsDao;
+  }
 }
